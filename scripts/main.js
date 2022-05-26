99b8edc5a561bf2cf2ee5ea7c3e7602c529e6725
Dipesh Gupta <dipesh.gupta@w3villa.com>
2022-05-27 00:22:17 +0530

    Modified css

--- scripts/main.js
+++ scripts/main.js
@@ -88,10 +88,15 @@ function runalgo()
 
 function bubbleFunction()
 {
+    const complexity = document.querySelector(".complex");
+    complexity.classList.add("active");
+
     document.getElementById("sorting-name").innerHTML = "Bubble Sort"
     document.getElementById("about-sort").innerHTML = "<br/>It is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in the wrong order."
 
-    document.getElementById("complexity").innerHTML = "<br/>Best Case Time Complexity O(n)<br/>Worst Case Time Complexity O(n^2)<br/>Average Case Time Complexity O(n^2)<br/>"
+    document.getElementById("best").innerHTML = "O(n)"
+    document.getElementById("worst").innerHTML = "O(n^2)"
+    document.getElementById("avg").innerHTML = "O(n^2)"
 
 }
 
@@ -100,7 +105,9 @@ function selectionFunction()
     document.getElementById("sorting-name").innerHTML = "Selection Sort"
     document.getElementById("about-sort").innerHTML = "<br/>This sorting algorithm is an in-place comparison-based algorithm in which the list is divided into two parts, the sorted part at the left end and the unsorted part at the right end. Initially, the sorted part is empty and the unsorted part is the entire list."
 
-    document.getElementById("complexity").innerHTML = "<br/>Best Case Time Complexity O(n)<br/>Worst Case Time Complexity O(n^2)<br/>Average Case Time Complexity O(n^2)<br/>"
+    document.getElementById("best").innerHTML = "O(n)"
+    document.getElementById("worst").innerHTML = "O(n^2)"
+    document.getElementById("avg").innerHTML = "O(n^2)"
 }
 
 function insertionFunction()
@@ -108,7 +115,9 @@ function insertionFunction()
     document.getElementById("sorting-name").innerHTML = "Insertion Sort"
     document.getElementById("about-sort").innerHTML = "<br/>This is an in-place comparison-based sorting algorithm. Here, a sub-list is maintained which is always sorted. <br/>For example, the lower part of an array is maintained to be sorted. An element which is to be 'insert'ed in this sorted sub-list, has to find its appropriate place and then it has to be inserted there. Hence the name, Insertion sort."
 
-    document.getElementById("complexity").innerHTML = "<br/>Best Case Time Complexity O(n)<br/>Worst Case Time Complexity O(n^2)<br/>Average Case Time Complexity O(n^2)<br/>"
+    document.getElementById("best").innerHTML = "O(n)"
+    document.getElementById("worst").innerHTML = "O(n^2)"
+    document.getElementById("avg").innerHTML = "O(n^2)"
 }
 
 function mergeFunction()
@@ -116,7 +125,9 @@ function mergeFunction()
     document.getElementById("sorting-name").innerHTML = "Merge Sort"
     document.getElementById("about-sort").innerHTML = "<br/>It is a sorting technique based on divide and conquer technique. Merge sort first divides the array into equal halves and then combines them in a sorted manner."
 
-    document.getElementById("complexity").innerHTML = "<br/>Best Case Time Complexity O(n*log(n))<br/>Worst Case Time Complexity O(n*log(n))<br/>Average Case Time Complexity O(n*log(n))"
+    document.getElementById("best").innerHTML = "O(n*log(n))"
+    document.getElementById("worst").innerHTML = "O(n*log(n))"
+    document.getElementById("avg").innerHTML = "O(n*log(n))"
 }
 
 function quickFunction()
@@ -124,7 +135,9 @@ function quickFunction()
     document.getElementById("sorting-name").innerHTML = "Quick Sort"
     document.getElementById("about-sort").innerHTML = "<br/>It is a highly efficient sorting algorithm and is based on partitioning of array of data into smaller arrays. <br/>A large array is partitioned into two arrays one of which holds values smaller than the specified value, say pivot, based on which the partition is made and another array holds values greater than the pivot value. <br/>Quicksort partitions an array and then calls itself recursively twice to sort the two resulting subarrays. "
 
-    document.getElementById("complexity").innerHTML = "<br/>Best Case Time Complexity O(n*log(n))<br/>Worst Case Time Complexity O(n*log(n))<br/>Average Case Time Complexity O(n^2)<br/>"
+    document.getElementById("best").innerHTML = "O(n*log(n))"
+    document.getElementById("worst").innerHTML = "O(n*log(n))"
+    document.getElementById("avg").innerHTML = "O(n^2)"
 }
 
 function heapFunction()
@@ -132,5 +145,7 @@ function heapFunction()
     document.getElementById("sorting-name").innerHTML = "Heap Sort"
     document.getElementById("about-sort").innerHTML = "<br/>Heap sort processes the elements by creating the min-heap or max-heap using the elements of the given array. Min-heap or max-heap represents the ordering of array in which the root element represents the minimum or maximum element of the array."
 
-    document.getElementById("complexity").innerHTML = "<br/>Best Case Time Complexity O(n*log(n))<br/>Worst Case Time Complexity O(n*log(n))<br/>Average Case Time Complexity O(n*log(n))<br/>"
+    document.getElementById("best").innerHTML = "O(n*log(n))"
+    document.getElementById("worst").innerHTML = "O(n*log(n))"
+    document.getElementById("avg").innerHTML = "O(n*log(n))"
 }
