ac6ec57959293e908298e8aa029cdc724d3d574e
Dipesh Gupta <dipesh.gupta@w3villa.com>
2022-05-27 00:26:24 +0530

    Some JS changes

--- scripts/main.js
+++ scripts/main.js
@@ -102,6 +102,9 @@ function bubbleFunction()
 
 function selectionFunction()
 {
+    const complexity = document.querySelector(".complex");
+    complexity.classList.add("active");
+
     document.getElementById("sorting-name").innerHTML = "Selection Sort"
     document.getElementById("about-sort").innerHTML = "<br/>This sorting algorithm is an in-place comparison-based algorithm in which the list is divided into two parts, the sorted part at the left end and the unsorted part at the right end. Initially, the sorted part is empty and the unsorted part is the entire list."
 
@@ -112,6 +115,9 @@ function selectionFunction()
 
 function insertionFunction()
 {
+    const complexity = document.querySelector(".complex");
+    complexity.classList.add("active");
+
     document.getElementById("sorting-name").innerHTML = "Insertion Sort"
     document.getElementById("about-sort").innerHTML = "<br/>This is an in-place comparison-based sorting algorithm. Here, a sub-list is maintained which is always sorted. <br/>For example, the lower part of an array is maintained to be sorted. An element which is to be 'insert'ed in this sorted sub-list, has to find its appropriate place and then it has to be inserted there. Hence the name, Insertion sort."
 
@@ -122,6 +128,9 @@ function insertionFunction()
 
 function mergeFunction()
 {
+    const complexity = document.querySelector(".complex");
+    complexity.classList.add("active");
+
     document.getElementById("sorting-name").innerHTML = "Merge Sort"
     document.getElementById("about-sort").innerHTML = "<br/>It is a sorting technique based on divide and conquer technique. Merge sort first divides the array into equal halves and then combines them in a sorted manner."
 
@@ -132,6 +141,9 @@ function mergeFunction()
 
 function quickFunction()
 {
+    const complexity = document.querySelector(".complex");
+    complexity.classList.add("active");
+
     document.getElementById("sorting-name").innerHTML = "Quick Sort"
     document.getElementById("about-sort").innerHTML = "<br/>It is a highly efficient sorting algorithm and is based on partitioning of array of data into smaller arrays. <br/>A large array is partitioned into two arrays one of which holds values smaller than the specified value, say pivot, based on which the partition is made and another array holds values greater than the pivot value. <br/>Quicksort partitions an array and then calls itself recursively twice to sort the two resulting subarrays. "
 
@@ -142,6 +154,9 @@ function quickFunction()
 
 function heapFunction()
 {
+    const complexity = document.querySelector(".complex");
+    complexity.classList.add("active");
+
     document.getElementById("sorting-name").innerHTML = "Heap Sort"
     document.getElementById("about-sort").innerHTML = "<br/>Heap sort processes the elements by creating the min-heap or max-heap using the elements of the given array. Min-heap or max-heap represents the ordering of array in which the root element represents the minimum or maximum element of the array."
 
